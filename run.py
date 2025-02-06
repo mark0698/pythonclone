e8791d76707ef65509c5133036708d18a14f23dd
mark0698 <markokeeffe98@gmail.com>
2025-02-06 04:17:28 +0000

    Add Opening Menu

--- run.py
+++ run.py
@@ -15,4 +15,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Python Project')
 
+def get_habit_data():
+    """
+    Get habit data for the user.
+    """
+    print("Please Enter an option below")
+    print("1. Enter a new habit")
+    print("2. View all habits")
+    print("4. Add a habit")
+    print("5. Delete a habit")
+    print("6. Exit\n")
+    data_str = input("Enter your Choice here: ")
+    print(f"The data provided is {data_str}")
 
+
+get_habit_data()
